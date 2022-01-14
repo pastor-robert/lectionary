a9bc63e9f875924a3bbd64039cc7eb526b2bcec5
Rob Adams <rob@rob-adams.us>
2022-01-14 17:53:37 +0000

    working Eve version

---
+++ settings.py
@@ -0,0 +1,90 @@
+# DOMAIN = {'people': {}}
+MONGO_URI = 'mongodb+srv://rob:MOXK054bVVWal67F@cluster0.p4xbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+
+schema = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/pyeve/cerberus) for details.
+    'firstname': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 10,
+    },
+    'lastname': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 15,
+        'required': True,
+        # talk about hard constraints! For the purpose of the demo
+        # 'lastname' is an API entry-point, so we need it to be unique.
+        'unique': True,
+    },
+    # 'role' is a list, and can only contain values from 'allowed'.
+    'role': {
+        'type': 'list',
+        'allowed': ["author", "contributor", "copy"],
+    },
+    # An embedded 'strongly-typed' dictionary.
+    'location': {
+        'type': 'dict',
+        'schema': {
+            'address': {'type': 'string'},
+            'city': {'type': 'string'}
+        },
+    },
+    'born': {
+        'type': 'datetime',
+    },
+    }
+
+people = {
+    # 'title' tag used in item links. Defaults to the resource title minus
+    # the final, plural 's' (works fine in most cases but not for 'people')
+    'item_title': 'person',
+
+    # by default the standard item entry point is defined as
+    # '/people/<ObjectId>'. We leave it untouched, and we also enable an
+    # additional read-only entry point. This way consumers can also perform
+    # GET requests at '/people/<lastname>'.
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'lastname'
+    },
+
+    # We choose to override global cache-control directives for this resource.
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+
+    # most global settings can be overridden at resource level
+    'resource_methods': ['GET', 'POST'],
+
+    'schema': schema
+}
+
+DOMAIN = {
+    'people': people,
+    }
+
+# Please note that MONGO_HOST and MONGO_PORT could very well be left
+# out as they already default to a bare bones local 'mongod' instance.
+#MONGO_HOST = 'cluster0.p4xbd.mongodb.net'
+#MONGO_PORT = 27017
+
+# Skip this block if your db has no auth. But it really should.
+#MONGO_USERNAME = 'rob'
+#MONGO_PASSWORD = 'Password12!'
+
+# Name of the database on which the user can be authenticated,
+# needed if --auth mode is enabled.
+#MONGO_AUTH_SOURCE = 'apitest'
+
+#MONGO_DBNAME = 'apitest'
